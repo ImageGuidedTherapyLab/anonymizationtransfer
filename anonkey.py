4c03aefc622966a3f9304d1923635d6801cee176
David Fuentes <fuentesdt@gmail.com>
2023-03-14 17:36:26 -0500

    ENH: anonymization key

---
+++ anonkey.py
@@ -0,0 +1,52 @@
+import sqlite3
+import os
+import csv
+anonconn = sqlite3.connect('/home/fuentes/cmdanon/ctkDICOM.sql')
+phiconn  = sqlite3.connect('/home/fuentes/cmdtest/ctkDICOM.sql')
+
+# get data from anonymized db
+cursor = anonconn.execute('''
+select pt.UID,pt.PatientID,st.StudyInstanceUID,st.StudyDate,se.SeriesInstanceUID,im.SOPInstanceUID, replace(im.Filename, '/home/fuentes/cmdanon/dicom/','') as phiUID
+                                       from Patients pt join Studies  st on pt.UID= st.PatientsUID 
+                                       join Series  se on se.StudyInstanceUID= st.StudyInstanceUID 
+                                       join images   im on se.seriesInstanceUID= im.seriesInstanceUID;
+''')
+anonymizekey = [ row[:-1] + tuple(row[-1].split('/')) for row in cursor]
+# get phi from anonymized db
+cursor = phiconn.execute('''
+select pt.UID,pt.PatientID,st.StudyInstanceUID,st.StudyDate,se.SeriesInstanceUID,im.SOPInstanceUID
+                                       from Patients pt join Studies  st on pt.UID= st.PatientsUID 
+                                       join Series  se on se.StudyInstanceUID= st.StudyInstanceUID 
+                                       join images   im on se.seriesInstanceUID= im.seriesInstanceUID ;
+''')
+phikey = [ row for row in cursor]
+
+try:
+   os.remove('./anonymization.sql')
+except FileNotFoundError:
+   pass
+workconn = sqlite3.connect('./anonymization.sql')
+cursor = workconn.cursor()
+cursor.execute("CREATE TABLE anonkey(UID,MRN,StudyInstanceUID,StudyDate,SeriesInstanceUID,SOPInstanceUID,phiStudyInstanceUID,phiSeriesInstanceUID,phiSOPInstanceUID)")
+cursor.executemany("INSERT INTO anonkey VALUES(?,?,?,?,?,?,?,?,?)", anonymizekey )
+workconn.commit()  # Remember to commit the transaction after executing INSERT
+cursor.execute("CREATE TABLE phikey(UID,MRN,StudyInstanceUID,StudyDate,SeriesInstanceUID,SOPInstanceUID)")
+cursor.executemany("INSERT INTO phikey VALUES(?,?,?,?,?,?)", phikey )
+workconn.commit()  # Remember to commit the transaction after executing INSERT
+cursor = workconn.execute('''
+create table anonymizekey  as
+select ak.MRN,ak.StudyInstanceUID,ak.StudyDate,ak.SeriesInstanceUID,ak.SOPInstanceUID,ak.phiStudyInstanceUID,ak.phiSeriesInstanceUID,ak.phiSOPInstanceUID,pk.MRN as phiMRN, pk.StudyDate as phiStudyDate
+                 from anonkey ak join phikey  pk  on ak.phiStudyInstanceUID   = pk.StudyInstanceUID
+                                                 and ak.phiSeriesInstanceUID  = pk.SeriesInstanceUID
+                                                 and ak.phiSOPInstanceUID     = pk.SOPInstanceUID;
+''')
+cursor = workconn.execute('''
+select * from anonymizekey;
+''')
+
+with open('anonymizekey.csv', 'w') as csvfile:
+  csvwrite = csv.writer(csvfile, delimiter='\t', quotechar='|', quoting=csv.QUOTE_MINIMAL)
+  fileHeader =  ['MRN','StudyInstanceUID','StudyDate','SeriesInstanceUID','SOPInstanceUID','phiStudyInstanceUID','phiSeriesInstanceUID','phiSOPInstanceUID','phiMRN','phiStudyDate']
+  csvwrite.writerow(fileHeader )
+  for row in cursor:
+     csvwrite.writerow( row )
--- builddcmdb.py
+++ builddcmdb.py
@@ -1,14 +1,17 @@
 from __main__ import slicer
 
-print('db dir',sys.argv[1])
-print('input image dir',sys.argv[2])
+print('import',sys.argv[1])
+print('db dir',sys.argv[2])
+print('input image dir',sys.argv[3])
 # instantiate a new DICOM browser
 slicer.util.selectModule("DICOM")
 dicomBrowser = slicer.modules.DICOMWidget.dicomBrowser
-dicomBrowser.databaseDirectory = sys.argv[1] 
-# use dicomBrowser.ImportDirectoryCopy to make a copy of the files (useful for importing data from removable storage)
-dicomFilesDirectory = sys.argv[2] 
-dicomBrowser.importDirectory(dicomFilesDirectory, dicomBrowser.ImportDirectoryCopy)
+dicomBrowser.databaseDirectory = sys.argv[2] 
+dicomFilesDirectory = sys.argv[3]
+if( 'link' == sys.argv[1]):
+  dicomBrowser.importDirectory(dicomFilesDirectory, dicomBrowser.ImportDirectoryAddLink)
+elif( 'copy' == sys.argv[1]):
+  dicomBrowser.importDirectory(dicomFilesDirectory, dicomBrowser.ImportDirectoryCopy)
 # wait for import to finish before proceeding (optional, if removed then import runs in the background)
 #dicomBrowser.waitForImportFinished()
 sys.exit(0)
